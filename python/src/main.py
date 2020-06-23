6fa04866c9716240971f84b45294006e8186bb82
kwatch <kwatch@gmail.com>
2020-06-23 11:58:16 +0900

    rewrite 'stof100()' entirely

--- python/src/main.py
+++ python/src/main.py
@@ -32,23 +32,15 @@ def add_edge(start, end, distance):
 # 123.4 -> 12340
 # .5 -> 50
 def stof100(s):
-	result = 0
-	place = 2
-	is_decimal_place = False
-	for ch in s:
-		if ch == '.':
-			is_decimal_place = True
-			continue
-		result *= 10
-		result += ord(ch) - ord('0')
-		if is_decimal_place:
-			place -= 1
-			if place == 0:
-				break
-	if place > 0:  # omittable, because 10**0 == 1
-		result *= (10 ** place)
-	return result
-
+	pair = s.split('.', 1)
+	if len(pair) == 1:
+		return int(pair[0] or 0) * 100
+	s1, s2 = pair
+	if len(s2) >= 2:
+		v = int(s2[0:2])
+	else:
+		v = int(s2 or 0) * 10
+	return int(s1 or 0) * 100 + v
 
 def load():
 	sys.stdin.readline()
