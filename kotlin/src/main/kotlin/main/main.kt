be7c45e2db5adce60ccbead14208ea38d65ea4b3
da_louis <ruaces@gmail.com>
2020-06-24 21:59:36 +0900

    Revert "kotlin: improve in/out performance."
    This reverts commit 46e379f0

--- kotlin/src/main/kotlin/main/main.kt
+++ kotlin/src/main/kotlin/main/main.kt
@@ -6,11 +6,9 @@ typealias NodeId = Int
 typealias NodeIndex = Int
 typealias Distance = Int
 
-const val DISTANCE_MULTIPLE = 100
+val DISTANCE_MULTIPLE = 100
 
 var is_debug = false
-
-val io = FastIO()
 
 class G(
 	var id2idx:MutableMap<NodeId,NodeIndex> = mutableMapOf<NodeId,NodeIndex>(),
@@ -64,16 +62,18 @@ fun stof100(s:String):Int {
 }
 
 fun load() {
-	io.readStringList(6) // skip header
+	readLine() // skip header
 
-	while (io.hasNext()) {
-		io.readString() // skip 1st elem
-		io.readString() // skip 2nd elem
-		val s = io.readInt()
-		val e = io.readInt()
-		io.readString() // skip 5th elem
-		val d = stof100(io.readString())
-		if (is_debug) io.println("s: $s e: $e D: $d")
+	while (true) {
+		val line = readLine()
+		if (line == null) {
+			break
+		}
+		val fields = line.split(",")
+		val s = fields[2].toInt()
+		val e = fields[3].toInt()
+		val d = stof100(fields[5])
+		if (is_debug) println("line: " + line + " s: " + s + " e: " + e + " D: " + d)
 		add_edge(s, e, d)
 	}
 }
@@ -99,7 +99,7 @@ fun dijkstra(start:NodeId, end:NodeId):Result {
 			continue
 		}
 		visited++
-		if (is_debug) io.println("visiting: $here distance: $distance")
+		if (is_debug) println("visiting: " + here + " distance: " + distance)
 
 		for ((to, weight) in g.edge[here]) {
 			val w = distance + weight
@@ -110,7 +110,7 @@ fun dijkstra(start:NodeId, end:NodeId):Result {
 			}
 		}
 	}
-	io.println("visited: $visited")
+	println("visited: "+ visited)
 
 	var n = e
 	val result = mutableListOf(g.idx2id[n])
@@ -124,11 +124,11 @@ fun dijkstra(start:NodeId, end:NodeId):Result {
 }
 
 fun main(args: Array<String>) {
-	val count = args[0].toInt()
+	val count = args[0].toInt() 
 	is_debug = args.size > 1 && args[1] == "debug"
 
 	load()
-	io.println("loaded nodes: " + g.idx)
+	println("loaded nodes: " + g.idx)
 
 	var distance: Distance
 	var route = listOf<NodeId>()
@@ -137,13 +137,12 @@ fun main(args: Array<String>) {
 		val result = dijkstra(s, g.idx2id[1])
 		distance = result.first
 		route = result.second
-		io.println("distance: $distance")
+		println("distance: " + distance)
 	}
 
-	io.print("route: ")
+	print("route: ")
 	for (id in route) {
-		io.print("$id ")
+		print("" + id + " ")
 	}
-	io.println()
-	io.flush()
+	println()
 }
